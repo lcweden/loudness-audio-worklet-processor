dfd2460e76b622d780ec7a702fe438a80a509ea4
eden <198768181+lcweden@users.noreply.github.com>
2025-05-11 17:39:00 +0000

    refactor: simplify playground

---
+++ playground/index.tsx
@@ -0,0 +1,46 @@
+import { JSXElement } from 'solid-js';
+import { render } from 'solid-js/web';
+import './index.css';
+
+const root = document.getElementById('root');
+
+const app = (): JSXElement => {
+  const audioContext = new AudioContext({ sampleRate: 48000 });
+
+  async function handleFileChange(event: Event) {
+    const files = (event.target as HTMLInputElement).files ?? [];
+    const file = files[0];
+
+    if (!file) {
+      return;
+    }
+
+    const arrayBuffer = await file.arrayBuffer();
+    const audioBuffer = await audioContext.decodeAudioData(arrayBuffer);
+    const { numberOfChannels, length, sampleRate } = audioBuffer;
+    const offlineAudioBuffer = new OfflineAudioContext(numberOfChannels, length, sampleRate);
+    const moduleUrl = new URL('../lib/index.ts', import.meta.url);
+
+    await offlineAudioBuffer.audioWorklet.addModule(moduleUrl);
+
+    const sourceNode = new AudioBufferSourceNode(offlineAudioBuffer, { buffer: audioBuffer });
+    const workletNode = new AudioWorkletNode(offlineAudioBuffer, 'loudness-processor');
+
+    sourceNode.connect(workletNode).connect(offlineAudioBuffer.destination);
+    sourceNode.start();
+
+    workletNode.port.onmessage = (event: MessageEvent) => {
+      console.log(event.data);
+    };
+
+    await offlineAudioBuffer.startRendering();
+  }
+
+  return (
+    <main>
+      <input class="file-input" type="file" onChange={handleFileChange} />
+    </main>
+  );
+};
+
+render(() => app(), root!);
