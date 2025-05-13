f3deba502813813f8226127107bb450b2675496b
eden <198768181+lcweden@users.noreply.github.com>
2025-05-13 16:59:48 +0000

    chore: rename lib to src

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,33 @@
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig, UserConfig } from 'vite';
 import solid from 'vite-plugin-solid';
-import tailwindcss from '@tailwindcss/vite';
 
-const mode = process.env.VITE_MODE as 'lib' | 'demo' | 'playground';
+const mode = process.env.VITE_MODE as 'demo' | 'playground';
 
 let config: UserConfig;
 
-if (mode === 'lib') {
-  config = defineConfig({
-    build: {
-      lib: {
-        entry: 'lib/index.ts',
-        formats: ['es'],
-        fileName: 'loudness.worklet',
-      },
-      minify: true,
-      sourcemap: false,
-      outDir: 'dist/lib',
-      emptyOutDir: true,
-      copyPublicDir: false,
-    },
-  });
-} else {
+if (mode) {
   config = defineConfig({
     plugins: [solid(), tailwindcss()],
     server: { host: true },
     root: mode,
     publicDir: '../public',
     build: { outDir: `dist/${mode}`, emptyOutDir: true },
+  });
+} else {
+  config = defineConfig({
+    build: {
+      lib: {
+        entry: 'src/index.ts',
+        formats: ['es'],
+        fileName: 'loudness.worklet',
+      },
+      minify: true,
+      sourcemap: false,
+      outDir: 'lib',
+      emptyOutDir: true,
+      copyPublicDir: false,
+    },
   });
 }
 
