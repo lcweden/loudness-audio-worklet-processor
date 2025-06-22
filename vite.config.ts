652e780198fca52c45bbddb6fa0b2056a724359d
eden <198768181+lcweden@users.noreply.github.com>
2025-06-22 14:30:05 +0000

    feat: enhance banner generation in loudness.worklet.js with metadata from package.json

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import tailwindcss from '@tailwindcss/vite';
 import fs from 'node:fs';
 import { defineConfig, UserConfig } from 'vite';
 import solid from 'vite-plugin-solid';
-import { version } from './package.json';
+import { author, description, license, repository, version } from './package.json';
 
 const mode = process.env.VITE_MODE as 'demo' | 'playground';
 const isDev = (process.env.NODE_ENV as 'development' | 'production') === 'development';
@@ -45,11 +45,15 @@ if (mode) {
             const file = fs.readFileSync(path, { encoding: 'utf-8' });
             const banner = `
 /**
+ * ${description}
+ * 
  * @file loudness.worklet.js
  * @version ${version}
- * @author 
+ * @author ${author}
+ * @license ${license}
+ * @see ${repository.url}
  */
-`;
+`.trim();
 
             fs.writeFileSync(path, banner + '\r\n\r\n' + file, { encoding: 'utf-8' });
           },
