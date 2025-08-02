5e0a24ff00c0330517a906e50952c4a3b54db301
eden <198768181+lcweden@users.noreply.github.com>
2025-08-02 14:32:28 +0000

    feat: implement service worker registration

--- demo/app.tsx
+++ demo/app.tsx
@@ -1,4 +1,17 @@
+import { onMount } from "solid-js";
+
 function App() {
+  onMount(() => {
+    const isDevMode = import.meta.env.MODE === "development";
+    const isServiceWorkerSupported = "serviceWorker" in navigator;
+
+    if (isDevMode || !isServiceWorkerSupported) return;
+
+    const serviceWorkerURL = new URL("./service-worker.ts", import.meta.url);
+    const serviceWorkerOptions = { type: "module" as WorkerType, scope: "/loudness-audio-worklet-processor/" };
+    navigator.serviceWorker.register(serviceWorkerURL, serviceWorkerOptions);
+  });
+
   return <div>App</div>;
 }
 
